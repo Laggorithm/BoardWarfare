e17e2a446d3d2e104daa9cc86986ee0696857871
Polina <149363845+Teaglum@users.noreply.github.com>
2024-10-24 10:23:19 +0300

    menu
    Main menu
    Some toggles

---
+++ BoardWarfare/Assets/Scripts/EconomyManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EconomyManager : MonoBehaviour
+{
+    public int playerGold = 100; // Золото игрока
+    public int enemyGold = 100; // Золото врага
+    public TextMeshProUGUI goldText; // UI для золота игрока
+    public TextMeshProUGUI goldText2;
+    void Start()
+    {
+        UpdateGoldUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdateGoldUI()
+    {
+        goldText.text = "Gold: " + playerGold;
+        goldText2.text = "Gold: " + playerGold;
+    }
+}
